eb922e3600ac1ad77a12f87200ba03ec02274e0d
Mustafa Zafar <zafarm@geomet-dev-31.edc-mtl.ec.gc.ca>
2025-03-17 20:15:56 +0000

    feature tests rough done

--- tests/integration.py
+++ tests/integration.py
@@ -36,6 +36,7 @@ from jsonschema import Draft202012Validator
 from jsonschema import exceptions
 from referencing import Registry, Resource, jsonschema
 # from referencing.exceptions import ReferenceResolutionError
+import json
 import os
 
 @pytest.fixture()
@@ -43,75 +44,75 @@ def url(pytestconfig):
     return pytestconfig.getoption('url')
 
 
-def est_msc_pygeoapi_service_online(url):
+# def est_msc_pygeoapi_service_online(url):
 
-    # Define the base directory
-    base_dir = os.path.abspath('tests/test-files/schemasCov/common-geodata')
-    print('Base dir:', base_dir)
+#     # Define the base directory
+#     base_dir = os.path.abspath('tests/test-files/schemasCov/common-geodata')
+#     print('Base dir:', base_dir)
 
-    # Load the schema_a.yaml file (in dir_a)
-    schema_a_path = os.path.join(base_dir, 'collectionDesc.yaml')
-    print('Schema a path:', schema_a_path)
-    with open(schema_a_path, 'r') as f:
-        schema_a = yaml.safe_load(f)
-        print("hereeee", schema_a)
+#     # Load the schema_a.yaml file (in dir_a)
+#     schema_a_path = os.path.join(base_dir, 'collectionDesc.yaml')
+#     print('Schema a path:', schema_a_path)
+#     with open(schema_a_path, 'r') as f:
+#         schema_a = yaml.safe_load(f)
+#         print("hereeee", schema_a)
 
-    # Load the other schema_b.yaml file (in dir_b)
-    schema_b_path = os.path.join(base_dir, '../common-core/link.yaml')
-    print('Schema b path:', schema_b_path)
-    with open(schema_b_path, 'r') as f:
-        schema_b = yaml.safe_load(f)
-        print("hereeee", schema_b)
+#     # Load the other schema_b.yaml file (in dir_b)
+#     schema_b_path = os.path.join(base_dir, '../common-core/link.yaml')
+#     print('Schema b path:', schema_b_path)
+#     with open(schema_b_path, 'r') as f:
+#         schema_b = yaml.safe_load(f)
+#         print("hereeee", schema_b)
 
-    # Load the other schema_c.yaml file (in dir_c)
-    schema_c_path = os.path.join(base_dir, 'extent-uad.yaml')
-    print('Schema c path:', schema_c_path)
-    with open(schema_c_path, 'r') as f:
-        schema_c = yaml.safe_load(f)
-        print("hereeee", schema_c)
+#     # Load the other schema_c.yaml file (in dir_c)
+#     schema_c_path = os.path.join(base_dir, 'extent-uad.yaml')
+#     print('Schema c path:', schema_c_path)
+#     with open(schema_c_path, 'r') as f:
+#         schema_c = yaml.safe_load(f)
+#         print("hereeee", schema_c)
 
-    # Load the other schema_d.yaml file (in dir_d)
-    schema_d_path = os.path.join(base_dir, 'extent.yaml')
-    print('Schema d path:', schema_d_path)
-    with open(schema_d_path, 'r') as f:
-        schema_d = yaml.safe_load(f)
-        print("hereeee", schema_d)
-
-
-
-    # resource for schema a
-    resourceA = Resource(contents=schema_a, specification=jsonschema.DRAFT202012)
-    # resource for schema b
-    resourceB = Resource(contents=schema_b, specification=jsonschema.DRAFT202012)
-    # resource for schema c
-    resourceC = Resource(contents=schema_c, specification=jsonschema.DRAFT202012)
-    # resource for schema c
-    resourceD = Resource(contents=schema_d, specification=jsonschema.DRAFT202012)
-
-    registry = Registry().with_resources([(f'{base_dir}', resourceA), ('../common-core/link.yaml', resourceB), ('extent-uad.yaml', resourceC), ('extent.yaml', resourceD)])
-
-    # Register the schema a in the registry as a resource
-    # registry = Registry().with_resource(uri=f'file://{base_dir}/', resource=resource)
-    print(registry)
-    registry = registry.crawl()
-    print(registry)
-
-    print(registry.contents(f'{base_dir}'))
+#     # Load the other schema_d.yaml file (in dir_d)
+#     schema_d_path = os.path.join(base_dir, 'extent.yaml')
+#     print('Schema d path:', schema_d_path)
+#     with open(schema_d_path, 'r') as f:
+#         schema_d = yaml.safe_load(f)
+#         print("hereeee", schema_d)
 
 
-    validator = Draft202012Validator(schema_a, registry=registry)
+
+#     # resource for schema a
+#     resourceA = Resource(contents=schema_a, specification=jsonschema.DRAFT202012)
+#     # resource for schema b
+#     resourceB = Resource(contents=schema_b, specification=jsonschema.DRAFT202012)
+#     # resource for schema c
+#     resourceC = Resource(contents=schema_c, specification=jsonschema.DRAFT202012)
+#     # resource for schema c
+#     resourceD = Resource(contents=schema_d, specification=jsonschema.DRAFT202012)
+
+#     registry = Registry().with_resources([(f'{base_dir}', resourceA), ('../common-core/link.yaml', resourceB), ('extent-uad.yaml', resourceC), ('extent.yaml', resourceD)])
+
+#     # Register the schema a in the registry as a resource
+#     # registry = Registry().with_resource(uri=f'file://{base_dir}/', resource=resource)
+#     print(registry)
+#     registry = registry.crawl()
+#     print(registry)
+
+#     print(registry.contents(f'{base_dir}'))
 
 
-    # Example instance to validate
-    # Make a basic request at the URL
-    response = requests.get(url, verify="/etc/ssl/certs")
-    instance = response.json()
+#     validator = Draft202012Validator(schema_a, registry=registry)
 
-    validator.validate(instance)
 
-    # Perform validation, resolving any $ref in the process
-    # for error in validator.iter_errors(instance):
-    #     print(f"Validation error: {error.message}")
+#     # Example instance to validate
+#     # Make a basic request at the URL
+#     response = requests.get(url, verify="/etc/ssl/certs")
+#     instance = response.json()
+
+#     validator.validate(instance)
+
+#     # Perform validation, resolving any $ref in the process
+#     # for error in validator.iter_errors(instance):
+#     #     print(f"Validation error: {error.message}")
 
 def est_feature_collection_root(url):
 
@@ -322,7 +323,7 @@ def est_feature_collection_items(url):
     validator = Draft202012Validator(schema_a, registry=registry)
     validator.validate(instance)
 
-def test_feature_collection_single_item(url):
+def est_feature_collection_single_item(url):
 
     # test with url: https://geomet-dev-31-nightly.edc-mtl.ec.gc.ca/msc-pygeoapi/collections/climate-normals/items/1664.62.1?f=json
 
@@ -449,3 +450,47 @@ def test_feature_collection_single_item(url):
     print('here is the schema for validator', schema_b)
     validator = Draft202012Validator(schema_b, registry=registry)
     validator.validate(instance)
+
+def test_feature(url):
+    #test with this url:  https://geomet-dev-31-nightly.edc-mtl.ec.gc.ca/msc-pygeoapi/collections?f=json
+
+    response = requests.get(url, verify="/etc/ssl/certs")
+
+    instance = response.json()
+
+    assert response.status_code == 200
+
+    print(json.dumps(instance, indent=4))
+    print(instance['collections'][0]['title'])
+
+    for collection in instance['collections']:
+        if 'itemType' in collection and collection['itemType'] == 'feature':
+            print(collection['id'])
+            url1 = f"https://geomet-dev-31-nightly.edc-mtl.ec.gc.ca/msc-pygeoapi/collections/{collection['id']}?f=json"
+            url2 = f"https://geomet-dev-31-nightly.edc-mtl.ec.gc.ca/msc-pygeoapi/collections/{collection['id']}/items?limit=1&f=json"
+
+            try:
+
+                response2 = requests.get(url2, verify="/etc/ssl/certs")
+
+                instance2 = response2.json()
+                print(url1)
+                itemId = instance2['features'][0]['id']
+
+            except Exception as e:
+                # Catch any exception that occurs and handle it here
+                print(f"An error occurred: {e}")
+
+            url3 = f"https://geomet-dev-31-nightly.edc-mtl.ec.gc.ca/msc-pygeoapi/collections/{collection['id']}/items/{itemId}?f=json"
+
+            print(url1)
+            print(url2)
+            print(url3)
+            print('----------------')
+            print(itemId)
+            print('----------------')
+            est_feature_collection_root(url1)
+            est_feature_collection_items(url2)
+            est_feature_collection_single_item(url3)
+
+
